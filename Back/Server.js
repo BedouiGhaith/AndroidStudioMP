30817cfd3fa8cae523f3448bda680a532c7dc0a9
BedouiGhaith <98600281+BedouiGhaith@users.noreply.github.com>
2022-12-31 11:35:08 +0100

    a

--- Back/Server.js
+++ Back/Server.js
@@ -19,6 +19,8 @@ const databaseName = 'AndroidMP';
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 
+
+
 mongoose
     .connect(`mongodb://localhost:27017/${databaseName}`)
     .then(() => {
